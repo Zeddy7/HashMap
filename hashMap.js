cb4adbe3752145bb48314c2e25e1de670a4a2051
Daniel Aziegbe <danielaziegbe98@gmail.com>
2025-12-28 19:08:36 -0500

    Began hashMap function

---
+++ hashMap.js
@@ -0,0 +1,19 @@
+function HashMap() {
+   return {
+      capacity: 16,
+      loadFactor: 0.75,
+      size: 0,
+
+      hash(key) {
+         let hashCode = 0;
+         const primeNumber = 31;
+
+         for (let i = 0; i < key.length; i++) {
+            hashCode = primeNumber * hashCode + key.charCodeAt(i);
+            hashCode %= 16;
+         }
+
+         return hashCode;
+      },
+   };
+}
